0abb534f149337d9a703b9fc8d3ced3abcbf29fc
Jonn Alves <jonatas.alsantos@jsl.com.br>
2023-03-10 16:10:11 -0300

    Initial Commit

---
+++ webbview/webbviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  webbviewApp.swift
+//  webbview
+//
+//  Created by Jonn Alves on 10/03/23.
+//
+
+import SwiftUI
+
+@main
+struct webbviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
